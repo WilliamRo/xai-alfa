64f840540d0aed64cfe6e2464c23e4bc23c50bdb
WilliamRo <walienluo@gmail.com>
2022-05-09 22:00:27 +0800

    [M] added some tutorials

---
+++ 99-tutorials/01-pictor/03-pictor-signal/01-signal-viewer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from pictor import Pictor
+from pictor.objects import DigitalSignal
+
+
+p = Pictor.signal_viewer()
+
+# Generate fake signals
+x = np.linspace(0, 8*np.pi, num=1000)
+p.objects = [DigitalSignal.sinusoidal(x, omega=[4, 8, 8, 12, 4, 4],
+                                      max_truncate_ratio=0.5)]
+
+p.show()
+
+
+
+
+
