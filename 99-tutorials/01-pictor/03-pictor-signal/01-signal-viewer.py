579772473bd10aced3c930c5c6eae19886fc2bc9
William <luo.wei@zju.edu.cn>
2022-05-10 16:26:03 +0800

    [M] ...

--- 99-tutorials/01-pictor/03-pictor-signal/01-signal-viewer.py
+++ 99-tutorials/01-pictor/03-pictor-signal/01-signal-viewer.py
@@ -4,7 +4,7 @@ from pictor import Pictor
 from pictor.objects import DigitalSignal
 
 
-p = Pictor.signal_viewer()
+p = Pictor.signal_viewer(default_win_size=1000)
 
 # Generate fake signals
 x = np.linspace(0, 8*np.pi, num=1000)
