79bf4088eedcbf0234628797566be6f81f9ed92d
WilliamRo <walienluo@gmail.com>
2022-07-20 22:11:48 +0800

    ...

--- 99-tutorials/98-incubator/80-MDS/nanobox/nanobox.py
+++ 99-tutorials/98-incubator/80-MDS/nanobox/nanobox.py
@@ -3,10 +3,31 @@ from pictor import Pictor
 from pictor.plugins.timer import Timer
 from pictor.objects import ParticleSystem
 
+import matplotlib.pyplot as plt
+
 
 
 class NanoBox(Pictor, XNode, Timer):
-  pass
+
+  def __init__(self, title='NanoBox', figure_size=(6, 6)):
+    # Call parent's constructor
+    super(NanoBox, self).__init__(title, figure_size)
+
+    # Set plotter
+    self.add_plotter(self.show_particles)
+
+
+  # region: Properties
+
+  # endregion: Properties
+
+  # region: Plotters
+
+  def show_particles(self, x: ParticleSystem, ax: plt.Axes):
+    ax.set_title(f'x = {x}')
+
+  # endregion: Plotters
+
 
 
 
