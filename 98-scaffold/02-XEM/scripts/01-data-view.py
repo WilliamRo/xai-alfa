8571686ff80b65916f6b63c9503f0831f322981f
William <luo.wei@zju.edu.cn>
2022-09-30 17:12:51 +0800

    ...

---
+++ 98-scaffold/02-XEM/scripts/01-data-view.py
@@ -0,0 +1,30 @@
+from pictor import Pictor
+from topaz.utils.data.loader import load_image
+from xem.ui.omma import Omma
+
+import os
+import numpy as np
+
+
+
+# -----------------------------------------------------------------------------
+# Read data
+# -----------------------------------------------------------------------------
+file_path = r'../data/EMPIAR-10025/rawdata/micrographs/'
+file_path += r'14sep05c_c_00003gr_00014sq_00004hl_00004es_c.mrc'
+
+assert os.path.exists(file_path)
+x = np.array(load_image(file_path), copy=False).astype(np.float32)
+
+
+# -----------------------------------------------------------------------------
+# Show data in Omma
+# -----------------------------------------------------------------------------
+from pictor.plotters.retina import Retina
+
+om = Omma('Omma', figure_size=(7, 7))
+om.objects = [x]
+om.add_plotter(Retina())
+om.show()
+
+
---
+++ 98-scaffold/02-XEM/xem/ui/omma.py
@@ -0,0 +1,13 @@
+from pictor import Pictor
+
+
+
+
+class Omma(Pictor):
+  pass
+
+
+
+if __name__ == '__main__':
+  om = Omma()
+  om.show()
